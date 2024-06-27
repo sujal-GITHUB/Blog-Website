157cc5219d2733c3d5d7f5065186312a90d00eb1
Sujal Kumar <118468993+sujal-GITHUB@users.noreply.github.com>
2024-06-27 14:38:27 +0530

    Update Footer.jsx

--- website/src/components/Footer/Footer.jsx
+++ website/src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ function Footer() {
                     <div className="w-full p-6 md:w-1/2 lg:w-5/12">
                         <div className="flex h-full flex-col justify-between">
                             <div className="mb-4 inline-flex items-center">
-                                <img src="src/blogger-logo.png" alt="" width={100}/>
+                                <img src="/blogger-logo.png" alt="" width={100}/>
                             </div>
                             <div>
                                 <p className="text-sm text-gray-200">
